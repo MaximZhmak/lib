23c1655b7ec55e635dcbdf8605ef2c9486065569
Maxim Zhmak <Maxim Zhmak>
2017-09-01 11:10:25 +0300

    some printf

--- hello.cpp
+++ hello.cpp
@@ -4,10 +4,15 @@ int main()
 {
 	std::cout<<"Hello World!\n";
 	
+	//feature1
 	for (char ch = 'A'; ch <= 'Z'; ch++)
 	{
 		printf("%c ", ch);
 	}
 
+	//develop
+	printf("\ninto develop branch\n");
+	
+	
 	return 0;
 }
